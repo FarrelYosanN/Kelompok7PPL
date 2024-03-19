0521a3292146da0c69c928be99bd245d7074383f
22091397032FarrelYosanNavyansyah <124505656+FarrelYosanN@users.noreply.github.com>
2024-03-19 19:52:08 +0700

    Add files via upload

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts.index', compact('contacts'));
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:contacts',
+            'phone' => 'required',
+        ]);
+
+        Contact::create($request->all());
+
+        return redirect()->route('contacts.index')
+            ->with('success', 'Contact created successfully.');
+    }
+
+    public function show($id)
+    {
+        $contact = Contact::findOrFail($id);
+        return view('contacts.show', compact('contact'));
+    }
+
+    public function edit($id)
+    {
+        $contact = Contact::findOrFail($id);
+        return view('contacts.edit', compact('contact'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:contacts,email,' . $id,
+            'phone' => 'required',
+        ]);
+
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+
+        return redirect()->route('contacts.index')
+            ->with('success', 'Contact updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return redirect()->route('contacts.index')
+            ->with('success', 'Contact deleted successfully.');
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+
+        $contacts = Contact::where('first_name', 'LIKE', "%$search%")
+            ->orWhere('last_name', 'LIKE', "%$search%")
+            ->orWhere('email', 'LIKE', "%$search%")
+            ->orWhere('phone', 'LIKE', "%$search%")
+            ->get();
+
+        return view('contacts.index', compact('contacts'));
+    }
+}
---
+++ resources/views/addresses/show.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('body')
+    <h1 class="mb-0">Address Details</h1>
+    <hr />
+    <div class="row">
+        <div class="col mb-3">
+            <label class="form-label">Street</label>
+            <input type="text" name="street" class="form-control" placeholder="street" value="{{ $address->street }}" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">City</label>
+            <input type="text" name="city" class="form-control" placeholder="city" value="{{ $address->city }}" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">Province</label>
+            <input type="text" name="province" class="form-control" placeholder="province" value="{{ $address->province }}" readonly>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col mb-3">
+            <label class="form-label">Country</label>
+            <input type="text" name="country" class="form-control" placeholder="country" value="{{ $address->country }}" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">Postal code</label>
+            <input type="text" name="postal code" class="form-control" placeholder="postal code" value="{{ $address->postal_code }}" readonly>
+        </div>   
+    </div>
+    <div class="row">
+        <div class="col mb-3">
+            <label class="form-label">Created At</label>
+            <input type="text" name="created_at" class="form-control" placeholder="Created At" value="{{ $address->created_at }}" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">Updated At</label>
+            <input type="text" name="updated_at" class="form-control" placeholder="Updated At" value="{{ $address->updated_at }}" readonly>
+        </div>
+    </div>
+    <!-- <div class="row mb-3">
+        <div class="col mb-3">
+            <strong>Street:</strong> {{ $address->street }}
+        </div>
+        <div class="col mb-3">
+            <strong>City:</strong> {{ $address->city }}
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col mb-3">
+            <strong>Province:</strong> {{ $address->province }}
+        </div>
+        <div class="col mb-3">
+            <strong>Country:</strong> {{ $address->country }}
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col mb-3">
+            <strong>Postal Code:</strong> {{ $address->postal_code }}
+        </div>
+    </div> -->
+    <div class="row">
+        <div class="col">
+            <a href="{{ route('addresses.index') }}" class="btn btn-secondary">Back</a>
+        </div>
+    </div>
+@endsection
